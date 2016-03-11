fd029e925a0c32e6d27da184999da7faeeafc65d
Morgan Waites <morgan.waites@gmail.com>
2016-03-11 01:26:55 -0800

    added search methods cleaned database table cities to match table rents

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template, request
 import jinja2
+import os
+import Quandl
 from search import Search
+import sqlite3
+import pandas 
+
 
 app = Flask(__name__)
 
+# API Stuff
+quandl_api_key = os.environ['QUANDL_API_KEY']
+
 app.jinja_env.undefined = jinja2.StrictUndefined
+
+sd = Search("Registered Nurse")
+a = sd.avg_salary()
+over = sd.over_med(a[0])
+added = sd.add_codes(over) 
+#will return cleaned list of lists containing location, occupation, median salary, number employed, and codes for rent added at end
+clean = sd.remove_dup(added)
+
+def best_list(big):
+	for i in xrange(len(big)):
+		listy=big[i]
+	 	code = listy[4]
+	 	code = 'ZILL/M' + code + "_RMP"
+	 	data = Quandl.get(code, authtoken=quandl_api_key)
+	 	last = data.tail(1)
+	 	rent = int(last.Value)
+	 	listy.append(rent)	
+	 	listy.append(listy[2] - (12*listy[5]))
+	return (big)
+
+best = best_list(clean)
+for item in best:
+	print item
 
 @app.route("/")
 def homepage():
--- search.py
+++ search.py
@@ -3,24 +3,77 @@ class Search:
 
 	def __init__(self, occupation):
 	    self.occupation = str(occupation)
+
+	#returns tuple containing avg salary of all salaries for given occupation in table
 	def avg_salary(self):
 		con = sqlite3.connect('data/data.db')
 		curs = con.cursor()
 		curs.execute("select avg(med) from tbl2 where occu LIKE ?", ('%'+self.occupation+'%',))
-		salary_tuple = curs.fetchone()
-		curs.execute("select occu from tbl2 where occu LIKE ?", ('%'+self.occupation+'%',))
-		occu = curs.fetchone();
+		salary_tuple = curs.fetchone() #fetch the first value it returns, since we only need one value
+		curs.execute("select occu from cities where occu LIKE ?", ('%'+self.occupation+'%',))
+
+		#obtain name of occupation as seem on table, so can print it correctly of results page (this is done in case user enters only part of name)
+		occu = curs.fetchone(); 
+		#returns tuple, must index to obtain text by itself
 		self.occupation= occu[0]
-		self.avg_salary = int(round(salary_tuple[0]))
-		tup = (self.avg_salary, self.occupation)
+		#same with average salary, must isolate integer from tuple given as fetchone() returns tuple
+		self.avg_salary = int(round(salary_tuple[0]))		
+		tup = (self.avg_salary, self.occupation) 
 		return (tup)
 
-	def over_med(self):
+	#returns list of tuples containing info of each row where salary of given occupation is higher than national average
+	def over_med(self, avg):
 		con = sqlite3.connect('data/data.db')
 		curs = con.cursor()
-	 	curs.execute("select locu,occu,med,emp from cities where occu LIKE ? AND med>=?", ('%'+self.occupation+'%', self.avg_salary))
+	 	curs.execute("select locu,occu,med,emp from cities where occu LIKE ? AND med>?", ('%'+self.occupation+'%', avg))
 	 	self.results=curs.fetchall()
 	 	return (self.results)
+	def add_codes(self, med_set):
+		con=sqlite3.connect("data/data.db")
+		cur =con.cursor()
+		over_set = med_set
+		for i in range(len(over_set)):
+			tup =over_set[i]
+			location = tup[0]
+			cur.execute("SELECT code from rents WHERE city=?", (location,))
+			code= cur.fetchone()
+			list_tup = list(tup)
+			if code != None:
+				code = code[0]
+			list_tup.append(code)
+			over_set[i]=list_tup
+		i=0
+		while i<(len(over_set)):
+			l = over_set[i]
+		 	if l[4] == None:
+		 		check = over_set.pop(i)
+		 	else:
+		 		i=i+1
+		over_set.sort(key=lambda x: x[2], reverse=True)
 
-	def cpi(self):
-		return 0
+		over_set= over_set[0:50]
+		self.over_set = over_set
+		return self.over_set
+	def remove_dup(self, listy):
+		checker=[]	
+		i=0	
+		while i<(len(listy)):
+			lil=listy[i]
+			locu=lil[0]
+			if locu not in checker:
+				checker.append(locu)
+				i=i+1
+			else:
+				listy.remove(lil)
+		return listy
+
+#TEST METHODS IN CLASS
+# test = Search("Software Developer")
+# a=test.avg_salary()
+# a= a[0]
+# print a
+# over = test.over_med(a)
+# clean = test.add_codes(over)
+# print clean
+
+
