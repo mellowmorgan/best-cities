f19a9658f3bc16fb9a38968776a9e83771ee4704
Morgan Waites <morgan.waites@gmail.com>
2016-03-17 15:19:06 -0700

    try with different port for heroku

--- main.py
+++ main.py
@@ -51,5 +51,5 @@ def occupation_handler(page):
 		return render_template('error.html')
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 33507))
+	app.run(host='0.0.0.0', port=port)
